a8eecf7abccf3bae00d7fc6a334993be551005cb
Student <example@example.com>
2020-02-19 16:29:23 -0800

    add all buttons with functions. get rippage...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Moment from 'moment';
+
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
@@ -11,14 +13,67 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      buffimuchiCharacter: 'null'
+      buffimuchiCharacter: {timeOpen: new Moment()}
     };
     this.handleNewBuffimuchi = this.handleNewBuffimuchi.bind(this);
+    this.handleRoid = this.handleRoid.bind(this);
+    this.handleRawTamagotchi = this.handleRawTamagotchi.bind(this);
+    this.handleSleep = this.handleSleep.bind(this);
+    this.handleTossWeight = this.handleTossWeight.bind(this);
+  }
+
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+    this.updateBuffimuchiAge(),
+    5000);
+  }
+
+  updateBuffimuchiAge() {
+    console.log("check");
+    let updateBuffimuchiCharacter = this.state.buffimuchiCharacter
+    updateBuffimuchiCharacter.energy = (updateBuffimuchiCharacter.energy + 1)
+    updateBuffimuchiCharacter.ripped = (updateBuffimuchiCharacter.ripped - 1)
+    updateBuffimuchiCharacter.happiness = (updateBuffimuchiCharacter.happiness + 1)
+    updateBuffimuchiCharacter.formattedWaitTime = (updateBuffimuchiCharacter.timeOpen).fromNow(true)
+    this.setState({buffimuchiCharacter: updateBuffimuchiCharacter})
   }
 
   handleNewBuffimuchi(newBuffimuchi) {
     newBuffimuchi.formattedWaitTime = (newBuffimuchi.timeOpen).fromNow(true);
     this.setState({buffimuchiCharacter: newBuffimuchi});
+  }
+
+  handleRoid() {
+    console.log("Roids!");
+    let updateBuffimuchiCharacter = this.state.buffimuchiCharacter
+    updateBuffimuchiCharacter.energy = (updateBuffimuchiCharacter.energy + 20)
+    updateBuffimuchiCharacter.ripped = (updateBuffimuchiCharacter.ripped + 10)
+    updateBuffimuchiCharacter.happiness = (updateBuffimuchiCharacter.happiness - 25)
+    this.setState({buffimuchiCharacter: updateBuffimuchiCharacter})
+  }
+
+  handleRawTamagotchi() {
+    let updateBuffimuchiCharacter = this.state.buffimuchiCharacter
+    updateBuffimuchiCharacter.energy = (updateBuffimuchiCharacter.energy + 15)
+    updateBuffimuchiCharacter.ripped = (updateBuffimuchiCharacter.ripped - 1)
+    updateBuffimuchiCharacter.happiness = (updateBuffimuchiCharacter.happiness + 15)
+    this.setState({buffimuchiCharacter: updateBuffimuchiCharacter})
+  }
+
+  handleSleep() {
+    let updateBuffimuchiCharacter = this.state.buffimuchiCharacter
+    updateBuffimuchiCharacter.energy = (updateBuffimuchiCharacter.energy + 60)
+    updateBuffimuchiCharacter.ripped = (updateBuffimuchiCharacter.ripped - 10)
+    updateBuffimuchiCharacter.happiness = (updateBuffimuchiCharacter.happiness + 10)
+    this.setState({buffimuchiCharacter: updateBuffimuchiCharacter})
+  }
+
+  handleTossWeight() {
+    let updateBuffimuchiCharacter = this.state.buffimuchiCharacter
+    updateBuffimuchiCharacter.energy = (updateBuffimuchiCharacter.energy - 20)
+    updateBuffimuchiCharacter.ripped = (updateBuffimuchiCharacter.ripped + 20)
+    updateBuffimuchiCharacter.happiness = (updateBuffimuchiCharacter.happiness - 1)
+    this.setState({buffimuchiCharacter: updateBuffimuchiCharacter})
   }
 
   render (){
@@ -36,7 +91,12 @@ class App extends React.Component {
 
 
 
-    <Route path='/Buffimuchi' render={()=><Buffimuchi buffimuchiCharacter={this.state.buffimuchiCharacter} />} />
+    <Route path='/Buffimuchi' render={()=><Buffimuchi
+      buffimuchiCharacter={this.state.buffimuchiCharacter}
+      onRawTamagotchi={this.handleRawTamagotchi}
+      onSleep={this.handleSleep}
+      onTossWeight={this.handleTossWeight}
+      onRoid={this.handleRoid} />} />
 
 
 
--- src/Buffimuchi.js
+++ src/Buffimuchi.js
@@ -3,27 +3,41 @@ import { Link } from 'react-router-dom';
 
 function Buffimuchi(props){
 
+//
+// function test() {
+//   console.log(`${props.buffimuchiCharacter.name}`);
+//   console.log(`${props.buffimuchiCharacter.primaryColor}`);
+//   console.log(`${props.buffimuchiCharacter.secondaryColor}`);
+//   console.log(`${props.buffimuchiCharacter.energy}`);
+//   console.log(`${props.buffimuchiCharacter.ripped}`);
+//   console.log(`${props.buffimuchiCharacter.happiness }`);
+//   console.log(`${props.buffimuchiCharacter.formattedWaitTime } OLD`);
+//   console.log(props);
+// }
 
-function test() {
-  console.log(`${props.buffimuchiCharacter.name}`);
-  console.log(`${props.buffimuchiCharacter.primaryColor}`);
-  console.log(`${props.buffimuchiCharacter.secondaryColor}`);
-  console.log(`${props.buffimuchiCharacter.energy}`);
-  console.log(`${props.buffimuchiCharacter.ripped}`);
-  console.log(`${props.buffimuchiCharacter.happiness }`);
-}
+
 
   return (
     <div className="buffimuchiClass">
-    <button onClick={test}>Click for console log.</button>
+
+    <button onClick={props.onRoid}>Get Juiced!</button>
+    <button onClick={props.onRawTamagotchi}>Eat Raw Tamagotchi!</button>
+    <button onClick={props.onSleep}>Sleep!</button>
+    <button onClick={props.onTossWeight}>Toss Weight Bra!</button>
 
     <h1>Your Buffimuchi</h1>
 
 
 
 <p>{props.buffimuchiCharacter.name}</p>
+<p>{props.buffimuchiCharacter.formattedWaitTime} OLD.</p>
 <p>{props.buffimuchiCharacter.primaryColor}</p>
 <p>{props.buffimuchiCharacter.secondaryColor}</p>
+<p>Energy: {props.buffimuchiCharacter.energy}</p>
+<p>Rippage: {props.buffimuchiCharacter.ripped}</p>
+<p>Happiness: {props.buffimuchiCharacter.happiness}</p>
+
+
 
     <style>{`
       .buffimuchiClass {
