5565c31bc29adb4da6155df60d62489c9c5b4870
Student <example@example.com>
2020-02-19 16:47:49 -0800

    add buffage to effect css div.

--- src/Buffimuchi.js
+++ src/Buffimuchi.js
@@ -3,17 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Buffimuchi(props){
 
-//
-// function test() {
-//   console.log(`${props.buffimuchiCharacter.name}`);
-//   console.log(`${props.buffimuchiCharacter.primaryColor}`);
-//   console.log(`${props.buffimuchiCharacter.secondaryColor}`);
-//   console.log(`${props.buffimuchiCharacter.energy}`);
-//   console.log(`${props.buffimuchiCharacter.ripped}`);
-//   console.log(`${props.buffimuchiCharacter.happiness }`);
-//   console.log(`${props.buffimuchiCharacter.formattedWaitTime } OLD`);
-//   console.log(props);
-// }
+let buffage = props.buffimuchiCharacter.ripped
 
 
 
@@ -25,11 +15,15 @@ function Buffimuchi(props){
     <button onClick={props.onSleep}>Sleep!</button>
     <button onClick={props.onTossWeight}>Toss Weight Bra!</button>
 
-    <h1>Your Buffimuchi</h1>
+    <h1>Your Buffimuchi: {props.buffimuchiCharacter.name}</h1>
 
 
 
-<p>{props.buffimuchiCharacter.name}</p>
+<div className="bicepClass">
+x {buffage}
+</div>
+
+
 <p>{props.buffimuchiCharacter.formattedWaitTime} OLD.</p>
 <p>{props.buffimuchiCharacter.primaryColor}</p>
 <p>{props.buffimuchiCharacter.secondaryColor}</p>
@@ -43,9 +37,15 @@ function Buffimuchi(props){
       .buffimuchiClass {
         height: 90vh;
         overflow: hidden;
-
         background-color: rgba(0, 0, 100, .1);
         text-align: center;
+      }
+      .bicepClass {
+        height: ${buffage}px;
+        border-radius: 100%;
+        width: 5%;
+        background-color: rgba(100, 0, 0, .1);
+
       }
       `}
       </style>
